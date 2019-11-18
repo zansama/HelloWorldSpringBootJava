2f97dd94f61be488ecfb06b8f12260ab5f049328
zan <clement.mortas@le-campus-numerique.fr>
2019-11-18 10:29:37 +0100

    first commit

---
+++ src/test/java/com/programclement/restservice/RestApp/RestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programclement.restservice.RestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
